8320e2ef9d00df4d60b475a480344e112d5739ba
Bernard Marius 't Hart <31785895+thartbm@users.noreply.github.com>
2018-03-07 11:02:21 -0500

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,4 @@
+# with functions that save and load experiment configurations - as JSON
+# functions that check which experiment configs are available and which participants already completed each experiment
+# one function that transforms the experiment config to an actual trial sequence when running a participant
+# and functions that store / update the state of a running experiment - also as JSON (or does this go to Exp.py?)
