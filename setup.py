100430d3c899232f51f7205777d3ffc82cdda12c
Bernard Marius 't Hart <mthart@yorku.ca>
2018-05-26 09:52:23 -0700

    had to *recursively* add files from app directory

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+import setuptools
 import py2exe
 
 import sys
@@ -7,7 +8,8 @@ import os
 
 #Initialize Holder Files
 preference_files = []
-#app_files = []
+app_files = []
+builder_files = []
 #my_data_files=matplotlib.get_py2exe_datafiles()
 
 #define which files you want to copy for data_files
@@ -15,16 +17,23 @@ for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\preferences
     f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
     preference_files.append(f1)
 
-#if you might need to import the app files
-#for files in os.listdir('C:\\Program Files\\PsychoPy2\\Lib\\site-packages\\PsychoPy-1.65.00-py2.6.egg\\psychopy\\app\\'):
-#    f1 = 'C:\\Program Files\\PsychoPy2\\Lib\\site-packages\\PsychoPy-1.65.00-py2.6.egg\\psychopy\\app\\' + files
-#    app_files.append(f1)
+###if you might need to import the app files
+##for files in filter(os.path.isfile, os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\')):
+##    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
+##    app_files.append(f1)
+##
+###adding builder files
+##for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\builder\\'):
+##    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\builder\\' + files
+##    builder_files.append(f1)
 
-#all_files = [("psychopy\\preferences", preference_files),("psychopy\\app", app_files), my_data_files[0]]
+app_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\') for f in filenames]
+
+all_files = [("psychopy\\preferences", preference_files),("psychopy\\app", app_files)]
 
 #combine the files
 #all_files = [("psychopy\\preferences", preference_files), my_data_files[0]]
-all_files = [("psychopy\\preferences", preference_files)]
+#all_files = [("psychopy\\preferences", preference_files)]
 
 
 #import matplotlib as mpl
@@ -35,7 +44,7 @@ setup(
     
     options = {
         "py2exe": {
-            "includes": ["pandas", "psychopy"],
+            "includes": ["pandas", "psychopy", "UserList", "UserString"],
             "dll_excludes":["MSVCP90.dll"],
             "excludes":["gevent._socket3"],
             "skip_archive": True,
