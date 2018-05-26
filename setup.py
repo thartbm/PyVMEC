86fd33f2e2294d8555cd76217d5c7fbcb289ab15
Bernard Marius 't Hart <mthart@yorku.ca>
2018-05-26 11:33:45 -0700

    pandas errors, mpl data file errors

--- setup.py
+++ setup.py
@@ -1,47 +1,49 @@
 from distutils.core import setup
 import setuptools
 import py2exe
+import matplotlib
 
 import sys
 sys.setrecursionlimit(3000)
 import os
+
+# pandas._libs.tslibs.conversion._TSObject should have an attribute __reduce_cython__
+# in versions of pandas > 0.20.0 (at least for Python < 3.4)
+# so: pip install pandas==0.20.0
 
 #Initialize Holder Files
 preference_files = []
 app_files = []
 builder_files = []
 lib23_files = []
-#my_data_files=matplotlib.get_py2exe_datafiles()
+pandas_libs_files = []
+
 
 #define which files you want to copy for data_files
 for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\'):
     f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\preferences\\' + files
     preference_files.append(f1)
 
-###if you might need to import the app files
-##for files in filter(os.path.isfile, os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\')):
-##    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\' + files
-##    app_files.append(f1)
-##
-###adding builder files
-##for files in os.listdir('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\builder\\'):
-##    f1 = 'C:\\Python27\\Lib\\site-packages\\psychopy\\app\\builder\\' + files
-##    builder_files.append(f1)
-
 app_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('C:\\Python27\\Lib\\site-packages\\psychopy\\app\\') for f in filenames]
 
 lib23_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('C:\\Python27\\Lib\\lib2to3\\') for f in filenames]
 
+pandas_libs_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk('C:\\Python27\\Lib\\site-packages\\pandas\\_libs\\') for f in filenames] 
 
-all_files = [("psychopy\\preferences", preference_files),("psychopy\\app", app_files), ('lib2to3', lib23_files)]
+
+all_files = [ ("psychopy\\preferences", preference_files),
+              ("psychopy\\app", app_files),
+              ('lib2to3', lib23_files),
+              ("pandas\\_lib", pandas_libs_files),
+              ]
 
 #combine the files
 #all_files = [("psychopy\\preferences", preference_files), my_data_files[0]]
 #all_files = [("psychopy\\preferences", preference_files)]
 
 
-#import matplotlib as mpl
-#data_files += mpl.get_py2exe_datafiles()
+import matplotlib as mpl
+all_files += mpl.get_py2exe_datafiles()
 
 #setup(options={"py2exe":{"dll_excludes":["MSVCP90.dll"]}}, console=['PyVMEC.py'], data_files=data_files)
 setup(
@@ -55,9 +57,11 @@ setup(
                          "scipy.sparse.csgraph._validation",
                          "scipy.linalg.cython_blas",
                          "scipy.linalg.cython_lapack",
+                         "scipy.linalg",
                          "scipy.special._ufuncs_cxx",
                          "scipy._lib.messagestream",
-                         "pyglet.resource"],
+                         "pyglet.resource",
+                         "PIL.Image"],
             "dll_excludes":["MSVCP90.dll"],
             "excludes":["gevent._socket3"],
             "skip_archive": True,
