f4e85f8b6ec6694148e1801f972186c5569b9951
thartbm <thartbm@gmail.com>
2018-05-28 10:32:02 -0400

    print any exception that might occur

--- PyVMEC.py
+++ PyVMEC.py
@@ -1,4 +1,7 @@
 # this is the run-time script that will make sure the whole GUI is started along with all other functionality
-import GUI
-
-GUI.start()
+try:
+    import GUI
+    GUI.start()
+except Exception as e:
+    print(e)
+    
